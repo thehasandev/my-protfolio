370509bb280d011697474c1f6ab647f0b6b2cc5c
Hasan <thehasandev@gmail.com>
2023-10-21 00:59:17 +0600

    problem

--- src/components/layouts/ProblemSlove.jsx
+++ src/components/layouts/ProblemSlove.jsx
@@ -6,19 +6,19 @@ import Flex from '../common/Flex'
 
 function ProblemSlove() {
   return (
-    <section className='mt-[120px]'>
+    <section className='md:mt-[120px] mt-[100px] px-4 xl:px-0'>
       <Container>
          <h2 className='font-dm font-bold text-secondary mb-8 text-[30px] text-center'>What sets me apart</h2>
-        <Flex className="gap-x-5 justify-center">
+        <Flex className="gap-x-5 justify-center flex-col md:flex-row">
 
-         <div className='w-[40%]'>
-            <div className='w-[400px]'>
+         <div className='lg:w-[40%]'>
+            <div className='md:w-[400px] w-[300px] mx-auto'>
             <Image src={Problem}/> 
             </div>
          </div>  
 
 
-         <div className='w-[40%]'>
+         <div className='lg:w-[40%]'>
             <h2 className='font-dm font-bold text-primary text-xl mt-5'>Problem-Solving Prowess, innovative Solutions and Collaborative Mindset</h2>
             <p className='font-dm font-medium text-secondary text-base mt-5'>I thrive on finding creative and innovative solutions to complex problems, pushing the boundaries of what's possible in web development.</p>
             <p className='font-dm font-medium text-secondary text-base mt-5'>tackling complex challenges and finding elegant solutions, turning obstacles into opportunities.</p>
--- src/components/layouts/Project.jsx
+++ src/components/layouts/Project.jsx
@@ -22,8 +22,8 @@ function Project() {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay:true,
-        prevArrow:<PrevArrow class=" bottom-10 right-1/2"/>,
-        nextArrow:<NextArrow class ="bottom-10 left-1/2"/>        
+        prevArrow:<PrevArrow class=" -bottom-12 md:bottom-10 right-1/2"/>,
+        nextArrow:<NextArrow class ="-bottom-12 md:bottom-10 left-1/2"/>        
 
       };
   return (
@@ -32,10 +32,10 @@ function Project() {
          <h2 className='font-dm font-bold text-secondary text-center text-[30px] mb-5'>My Current Project </h2> 
         
         <Slider {...settings}>
-          <div>
-             <div className='w-full  bg-white shadow-xl shadow-gray-400/20  px-8 pt-8 pb-[140px] rounded-[5px]'>
-                <Flex className="justify-between">
-                   <div className='w-[40%] relative overflow-hidden group'>
+        
+             <div className='md:w-full  bg-white shadow-xl shadow-gray-400/20  px-8 pt-8 md:pb-[140px] rounded-[5px]'>
+                <Flex className="justify-between gap-y-5 flex-col md:flex-row">
+                   <div className='md:w-[40%] relative overflow-hidden group'>
                     
                     <div className='scale-105 group '>
                       <Image src={ProjectOne} className="group-hover:scale-110 duration-500 "/>
@@ -47,14 +47,17 @@ function Project() {
                         <p className='font-dm font-medium text-white text-lg '>Live Project</p>
                      </div>
                    </div>
-                   <div className='w-[55%]'>
-                   <h2 className='font-dm font-bold text-primary text-[20px]'>Orebi Ecommarce</h2> 
-                    <p className='font-dm font-medium font-base text-secondary w-8/12'>
+
+                   <div className='md:w-[55%] md:text-left text-center'>
+                   <h2 className='font-dm font-bold mb-2 md:mb-0 text-primary text-[20px]'>Orebi Ecommarce</h2> 
+                    <p className='font-dm font-normal md:font-medium text-sm md:text-base text-secondary mx-auto text-center md:text-left md:pl-0 md:mx-0 w-8/12'>
                     Website where you can get famous citation APIs collected by different RestApis and create your own citations by setting by your personal preferences.
                     </p>
 
-                    <p className='mt-10 font-dm font-semibold font-lg text-secondary mb-5'>Use is Project :</p>
-                    <Flex className="gap-x-10  flex-wrap">
+                 <div className='hidden md:block'>
+                    <p className='mt-10 hidden md:block font-dm font-semibold font-lg text-secondary mb-5'>Use is Project :</p>
+                   
+                    <Flex className="md:gap-x-10  flex-wrap mt-4 md:mt-0 gap-2 justify-center">
                          <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
                              <p className='font-dm font-semibold font-lg text-secondary '>React</p>
                          </div>
@@ -72,27 +75,27 @@ function Project() {
                          </div>
                     </Flex>
 
-                    <Flex className="justify-center gap-x-4 pt-8">
-                        <Flex className=' items-center gap-x-2'>
-                            <p className='font-dm font-medium font-base text-primary'>Code</p>
-                            <AiFillGithub size={50}/>
+
+                        <Flex className="justify-center gap-x-4 pt-8 ">
+                            <Flex className='items-center gap-x-2'>
+                                <p className='font-dm font-medium font-base text-primary'>Code</p>
+                                <AiFillGithub size={30}/>
+                            </Flex>
+
+                            <Flex className=' items-center gap-x-2'>
+                                <p className='font-dm font-medium font-base text-primary'>Live Demo</p>
+                                <FiExternalLink size={25}/>
+                            </Flex>
+                        
                         </Flex>
 
-                        <Flex className=' items-center gap-x-2'>
-                            <p className='font-dm font-medium font-base text-primary'>Live Demo</p>
-                            <FiExternalLink size={30}/>
-                        </Flex>
-                    
-                    </Flex>
+                    </div>
                    </div>
                 </Flex>
              </div>
-          </div>
-
-          <div>
-            <div className='w-full   bg-white shadow-xl shadow-gray-400/20  px-8 pt-8 pb-[170px] rounded-[5px]'>
-                <Flex className="justify-between">
-                <div className='w-[35%] relative overflow-hidden group'>
+             <div className='md:w-full  bg-white shadow-xl shadow-gray-400/20  px-8 pt-8 md:pb-[140px] rounded-[5px]'>
+                <Flex className="justify-between gap-y-5 flex-col md:flex-row">
+                   <div className='md:w-[40%] relative overflow-hidden group'>
                     
                     <div className='scale-105 group '>
                       <Image src={ProjectTwo} className="group-hover:scale-110 duration-500 "/>
@@ -105,53 +108,54 @@ function Project() {
                      </div>
                    </div>
 
-                   <div className='w-[55%]'>
-                    <h2 className='font-dm font-bold text-primary text-[20px]'>Daraz Ecommarce</h2> 
-                        <p className='font-dm font-medium font-base text-secondary w-8/12'>
-                        Website where you can get famous citation APIs collected by different RestApis and create your own citations by setting by your personal preferences.
-                        </p>
+                   <div className='md:w-[55%] md:text-left text-center'>
+                   <h2 className='font-dm font-bold mb-2 md:mb-0 text-primary text-[20px]'>Daraz Ecommarce</h2> 
+                    <p className='font-dm font-normal md:font-medium text-sm md:text-base text-secondary mx-auto text-center md:text-left md:pl-0 md:mx-0 w-8/12'>
+                    Website where you can get famous citation APIs collected by different RestApis and create your own citations by setting by your personal preferences.
+                    </p>
 
-                        <p className='mt-10 font-dm font-semibold font-lg text-secondary mb-5'>Use is Project :</p>
-                        <Flex className="gap-x-10  flex-wrap">
-                            <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
-                                <p className='font-dm font-semibold font-lg text-secondary '>React</p>
-                            </div>
-                            <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
-                                <p className='font-dm font-semibold font-lg text-secondary '>Tailwind</p>
-                            </div>
-                            <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
-                                <p className='font-dm font-semibold font-lg text-secondary '>Javascript</p>
-                            </div>
-                            <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
-                                <p className='font-dm font-semibold font-lg text-secondary '>Redux</p>
-                            </div>
-                            <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
-                                <p className='font-dm font-semiboldfont-lg text-secondary '>Context Api</p>
-                            </div>
-                        </Flex>
-
-                    <Flex className="justify-center gap-x-4 pt-8">
-                        <Flex className=' items-center gap-x-2'>
-                            <p className='font-dm font-medium font-base text-primary'>Code</p>
-                            <AiFillGithub size={50}/>
-                        </Flex>
-
-                        <Flex className=' items-center gap-x-2'>
-                            <p className='font-dm font-medium font-base text-primary'>Live Demo</p>
-                            <FiExternalLink size={30}/>
-                        </Flex>
-                    
+                 <div className='hidden md:block'>
+                    <p className='mt-10 hidden md:block font-dm font-semibold font-lg text-secondary mb-5'>Use is Project :</p>
+                   
+                    <Flex className="md:gap-x-10  flex-wrap mt-4 md:mt-0 gap-2 justify-center">
+                         <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
+                             <p className='font-dm font-semibold font-lg text-secondary '>React</p>
+                         </div>
+                         <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
+                             <p className='font-dm font-semibold font-lg text-secondary '>Tailwind</p>
+                         </div>
+                         <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
+                             <p className='font-dm font-semibold font-lg text-secondary '>Javascript</p>
+                         </div>
+                         <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
+                             <p className='font-dm font-semibold font-lg text-secondary '>Redux</p>
+                         </div>
+                         <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
+                             <p className='font-dm font-semiboldfont-lg text-secondary '>Context Api</p>
+                         </div>
                     </Flex>
-                    
+
+
+                        <Flex className="justify-center gap-x-4 pt-8 ">
+                            <Flex className='items-center gap-x-2'>
+                                <p className='font-dm font-medium font-base text-primary'>Code</p>
+                                <AiFillGithub size={30}/>
+                            </Flex>
+
+                            <Flex className=' items-center gap-x-2'>
+                                <p className='font-dm font-medium font-base text-primary'>Live Demo</p>
+                                <FiExternalLink size={25}/>
+                            </Flex>
+                        
+                        </Flex>
+
+                    </div>
                    </div>
                 </Flex>
-            </div>
-          </div>
-
-          <div>
-            <div className='w-full  bg-white shadow-xl shadow-gray-400/20 px-8 pt-8 pb-[170px] rounded-[5px]'>
-                <Flex className="justify-between">
-                <div className='w-[35%] relative overflow-hidden group'>
+             </div>
+             <div className='md:w-full  bg-white shadow-xl shadow-gray-400/20  px-8 pt-8 md:pb-[140px] rounded-[5px]'>
+                <Flex className="justify-between gap-y-5 flex-col md:flex-row">
+                   <div className='md:w-[40%] relative overflow-hidden group'>
                     
                     <div className='scale-105 group '>
                       <Image src={ProjectThree} className="group-hover:scale-110 duration-500 "/>
@@ -164,48 +168,56 @@ function Project() {
                      </div>
                    </div>
 
-                   <div className='w-[55%]'>
-                    <h2 className='font-dm font-bold text-primary text-[20px]'>Security Guard Website</h2> 
-                        <p className='font-dm font-medium font-base text-secondary w-8/12'>
-                        Website where you can get famous citation APIs collected by different RestApis and create your own citations by setting by your personal preferences.
-                        </p>
+                   <div className='md:w-[55%] md:text-left text-center'>
+                   <h2 className='font-dm font-bold mb-2 md:mb-0 text-primary text-[20px]'>Security Gurd Website</h2> 
+                    <p className='font-dm font-normal md:font-medium text-sm md:text-base text-secondary mx-auto text-center md:text-left md:pl-0 md:mx-0 w-8/12'>
+                    Website where you can get famous citation APIs collected by different RestApis and create your own citations by setting by your personal preferences.
+                    </p>
 
-                        <p className='mt-10 font-dm font-semibold font-lg text-secondary mb-5'>Use is Project :</p>
-                        <Flex className="gap-x-10  flex-wrap">
-                            <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
-                                <p className='font-dm font-semibold font-lg text-secondary '>React</p>
-                            </div>
-                            <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
-                                <p className='font-dm font-semibold font-lg text-secondary '>Tailwind</p>
-                            </div>
-                            <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
-                                <p className='font-dm font-semibold font-lg text-secondary '>Javascript</p>
-                            </div>
-                            <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
-                                <p className='font-dm font-semibold font-lg text-secondary '>Redux</p>
-                            </div>
-                            <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
-                                <p className='font-dm font-semiboldfont-lg text-secondary '>Context Api</p>
-                            </div>
-                        </Flex>
-
-                    <Flex className="justify-center gap-x-4 pt-8">
-                        <Flex className=' items-center gap-x-2'>
-                            <p className='font-dm font-medium font-base text-primary'>Code</p>
-                            <AiFillGithub size={50}/>
-                        </Flex>
-
-                        <Flex className=' items-center gap-x-2'>
-                            <p className='font-dm font-medium font-base text-primary'>Live Demo</p>
-                            <FiExternalLink size={30}/>
-                        </Flex>
-                    
+                 <div className='hidden md:block'>
+                    <p className='mt-10 hidden md:block font-dm font-semibold font-lg text-secondary mb-5'>Use is Project :</p>
+                   
+                    <Flex className="md:gap-x-10  flex-wrap mt-4 md:mt-0 gap-2 justify-center">
+                         <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
+                             <p className='font-dm font-semibold font-lg text-secondary '>React</p>
+                         </div>
+                         <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
+                             <p className='font-dm font-semibold font-lg text-secondary '>Tailwind</p>
+                         </div>
+                         <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
+                             <p className='font-dm font-semibold font-lg text-secondary '>Javascript</p>
+                         </div>
+                         <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
+                             <p className='font-dm font-semibold font-lg text-secondary '>Redux</p>
+                         </div>
+                         <div className='px-4 py-2 bg-white shadow-xl shadow-gray-400/30 rounded-[5px]'>
+                             <p className='font-dm font-semiboldfont-lg text-secondary '>Context Api</p>
+                         </div>
                     </Flex>
 
+
+                        <Flex className="justify-center gap-x-4 pt-8 ">
+                            <Flex className='items-center gap-x-2'>
+                                <p className='font-dm font-medium font-base text-primary'>Code</p>
+                                <AiFillGithub size={30}/>
+                            </Flex>
+
+                            <Flex className=' items-center gap-x-2'>
+                                <p className='font-dm font-medium font-base text-primary'>Live Demo</p>
+                                <FiExternalLink size={25}/>
+                            </Flex>
+                        
+                        </Flex>
+
+                    </div>
                    </div>
                 </Flex>
-            </div>
-          </div>
+             </div>
+
+         
+        
+
+     
 
         </Slider>
 
