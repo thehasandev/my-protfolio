a3c3881c30603a958150d43fd0ed857371868e30
Hasan <thehasandev@gmail.com>
2023-10-21 12:03:19 +0600

    responsive

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -11,10 +11,10 @@ import { BiLogoLinkedin} from "react-icons/bi"
 
 function Footer() {
   return (
-    <section className='py-[60px] bg-gray-300 '>
+    <section className='py-[60px] bg-gray-300 px-4 xl:px-0'>
         <Container>
-            <Flex className="justify-between">
-                <div className='w-3/12'>
+            <Flex className="justify-center flex-wrap gap-y-10">
+                <div className='md:w-3/12 '>
                     <div className='w-[100px] mx-auto'>
                        <Image src={Man} className="rounded-full"/>
                     </div>
@@ -35,8 +35,9 @@ function Footer() {
                         </div>
                       </div>
                 </div>
-               
-                <div className='w-2/12'>
+               <Flex className="gap-x-10 md:gap-x-5 justify-center">
+
+                <div className='md:w-2/12 '>
                   <h3 className='font-dm font-bold text-lg text-primary mb-5 uppercase'>Menu</h3>
                   <ul>
                     <List text="Home" footer/>
@@ -46,7 +47,7 @@ function Footer() {
                   </ul>
                 </div>
 
-                <div className='w-2/12'>
+                <div className='md:w-2/12 '>
                   <h3 className='font-dm font-bold text-lg text-primary mb-5 uppercase'>Service</h3>
                   <ul>
                     <List text="Home" footer/>
@@ -56,7 +57,7 @@ function Footer() {
                   </ul>
                 </div>
 
-                <div className='w-2/12'>
+                <div className='md:w-2/12  hidden md:block'>
                   <h3 className='font-dm font-bold text-lg text-primary mb-5 uppercase'>BLOG</h3>
                   <ul>
                     <List text="Portfolio" footer/>
@@ -66,18 +67,19 @@ function Footer() {
                   </ul>
                 </div>
 
-                <div className='w-2/12'>
+                <div className='md:w-2/12 '>
                   <h3 className='font-dm font-bold text-lg text-primary mb-5 uppercase'>ABOUT ME</h3>
                   <ul>
-                    <p className='text-secondary'>Software Engineer by work. Grad Student. Class of '23. Gitkraken Ambassador Nepal, 2021/22.</p>
+                    <p className='text-secondary w-[100px] md:w-[auto] font-normal md:text-sm text-xs'>Software Engineer by work. Grad Student. Class of '23. Gitkraken Ambassador Nepal, 2021/22.</p>
                     
                   </ul>
                 </div>
+               </Flex>
                 
               
                 
             </Flex>
-            <p className='font-dm text-white text-sm font-thin text-right pt-5'>Copyright © Rajan Bhattarai 2022"Affiliates Theme" - Modified by @cdrrazan
+            <p className='font-dm text-primary text-xs md:text-sm font-normal text-right pt-5'>Copyright © Hasan 2023- Modified by @thehasandev
 </p>
         </Container>
 
